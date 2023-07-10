46e7abd6c0db872ccfd928c707053ba3da86069d
phamgialinhlx <phamgialinhlx2@gmail.com>
2023-07-10 21:02:30 +0700

    Add CUDA Callback

---
+++ src/utils/callback/CUDACallback.py
@@ -0,0 +1,29 @@
+from lightning import Callback, LightningDataModule, LightningModule, Trainer
+import time
+import torch
+import wandb
+from pytorch_lightning.utilities import rank_zero_info
+
+class CUDACallback(Callback):
+    # see https://github.com/SeanNaren/minGPT/blob/master/mingpt/callback.py
+    def on_train_epoch_start(self, trainer, pl_module):
+        # Reset the memory use counter
+        torch.cuda.reset_peak_memory_stats(trainer.root_gpu)
+        torch.cuda.synchronize(trainer.root_gpu)
+        self.start_time = time.time()
+
+    def on_train_epoch_end(self, trainer, pl_module: LightningModule, outputs):
+        torch.cuda.synchronize(trainer.root_gpu)
+        max_memory = torch.cuda.max_memory_allocated(trainer.root_gpu) / 2 ** 20
+        epoch_time = time.time() - self.start_time
+
+        try:
+            max_memory = trainer.training_type_plugin.reduce(max_memory)
+            epoch_time = trainer.training_type_plugin.reduce(epoch_time)
+
+            rank_zero_info(f"Average Epoch time: {epoch_time:.2f} seconds")
+            rank_zero_info(f"Average Peak memory {max_memory:.2f}MiB")
+            pl_module.log("Average Epoch time (seconds)", epoch_time, on_step=False, on_epoch=True)
+            pl_module.log("Average Peak memory (MiB)", max_memory, on_step=False, on_epoch=True)
+        except AttributeError:
+            pass
